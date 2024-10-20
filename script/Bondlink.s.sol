4bd2bb6476f77f137b7c7c2210e342180ecfdc0c
d1ceeth <erwinphanglius10@gmail.com>
2024-10-20 12:55:47 +0700

    fix: dependencies

--- script/Bondlink.s.sol
+++ script/Bondlink.s.sol
@@ -12,12 +12,12 @@ import { SPCTPriceOracle } from "../src/v1/SPCTPriceOracle.sol";
 import "../interfaces/ISPCTPool.sol";
 import "../interfaces/ISPCTPriceOracle.sol";
 
-// LayerZero imports
-import { ExecutorConfig } from "@layerzerolabs/lz-evm-messagelib-v2/contracts/SendLibBase.sol";
-import { ILayerZeroEndpointV2 } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
-import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
-import { SetConfigParam } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/IMessageLibManager.sol";
-import { UlnConfig } from "@layerzerolabs/lz-evm-messagelib-v2/contracts/uln/UlnBase.sol";
+// // LayerZero imports
+// import { ExecutorConfig } from "@layerzerolabs/lz-evm-messagelib-v2/contracts/SendLibBase.sol";
+// import { ILayerZeroEndpointV2 } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
+// import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+// import { SetConfigParam } from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/IMessageLibManager.sol";
+// import { UlnConfig } from "@layerzerolabs/lz-evm-messagelib-v2/contracts/uln/UlnBase.sol";
 
 contract DeployBondlinkTestnet is Script {
     function launch() public {
@@ -58,77 +58,77 @@ contract DeployBondlinkTestnet is Script {
         console2.log("contract deployed on with address: ", address(_susdb));
     }
 
-    function setLibrary(address _endpoint, address _oapp, uint32 _eid, address _sendLib, address _receiveLib) external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        // Initialize the endpoint contract
-        ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(_endpoint);
+    // function setLibrary(address _endpoint, address _oapp, uint32 _eid, address _sendLib, address _receiveLib) external {
+    //     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    //     // Initialize the endpoint contract
+    //     ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(_endpoint);
 
-        // Start broadcasting transactions
-        vm.startBroadcast(deployerPrivateKey);
+    //     // Start broadcasting transactions
+    //     vm.startBroadcast(deployerPrivateKey);
 
-        // Set the send library
-        endpoint.setSendLibrary(_oapp, _eid, _sendLib);
-        console.log("Send library set successfully.");
+    //     // Set the send library
+    //     endpoint.setSendLibrary(_oapp, _eid, _sendLib);
+    //     console.log("Send library set successfully.");
 
-        // Set the receive library
-        endpoint.setReceiveLibrary(_oapp, _eid, _receiveLib, 0);
-        console.log("Receive library set successfully.");
+    //     // Set the receive library
+    //     endpoint.setReceiveLibrary(_oapp, _eid, _receiveLib, 0);
+    //     console.log("Receive library set successfully.");
 
-        // Stop broadcasting transactions
-        vm.stopBroadcast();
-    }
+    //     // Stop broadcasting transactions
+    //     vm.stopBroadcast();
+    // }
 
-    uint32 public constant EXECUTOR_CONFIG_TYPE = 1;
-    uint32 public constant ULN_CONFIG_TYPE = 2;
+    // uint32 public constant EXECUTOR_CONFIG_TYPE = 1;
+    // uint32 public constant ULN_CONFIG_TYPE = 2;
 
-    function setSendConfig(address contractAddress, uint32 remoteEid, address sendLibraryAddress, UlnConfig calldata ulnConfig, ExecutorConfig calldata executorConfig) external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        OFT myOFT = OFT(contractAddress);
+    // function setSendConfig(address contractAddress, uint32 remoteEid, address sendLibraryAddress, UlnConfig calldata ulnConfig, ExecutorConfig calldata executorConfig) external {
+    //     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    //     OFT myOFT = OFT(contractAddress);
 
-        ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(address(myOFT.endpoint()));
+    //     ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(address(myOFT.endpoint()));
 
-        SetConfigParam[] memory setConfigParams = new SetConfigParam[](2);
+    //     SetConfigParam[] memory setConfigParams = new SetConfigParam[](2);
 
-        setConfigParams[0] = SetConfigParam({
-            eid: remoteEid,
-            configType: EXECUTOR_CONFIG_TYPE,
-            config: abi.encode(executorConfig)
-        });
+    //     setConfigParams[0] = SetConfigParam({
+    //         eid: remoteEid,
+    //         configType: EXECUTOR_CONFIG_TYPE,
+    //         config: abi.encode(executorConfig)
+    //     });
 
-        setConfigParams[1] = SetConfigParam({
-            eid: remoteEid,
-            configType: ULN_CONFIG_TYPE,
-            config: abi.encode(ulnConfig)
-        });
+    //     setConfigParams[1] = SetConfigParam({
+    //         eid: remoteEid,
+    //         configType: ULN_CONFIG_TYPE,
+    //         config: abi.encode(ulnConfig)
+    //     });
 
-        vm.startBroadcast(deployerPrivateKey);
+    //     vm.startBroadcast(deployerPrivateKey);
 
-        endpoint.setConfig(address(myOFT), sendLibraryAddress, setConfigParams);
+    //     endpoint.setConfig(address(myOFT), sendLibraryAddress, setConfigParams);
 
-        vm.stopBroadcast();
-    }
+    //     vm.stopBroadcast();
+    // }
 
 
-    uint32 public constant RECEIVE_CONFIG_TYPE = 2;
+    // uint32 public constant RECEIVE_CONFIG_TYPE = 2;
 
-    function setReceiveConfig(address contractAddress, uint32 remoteEid, address receiveLibraryAddress, UlnConfig calldata ulnConfig) external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        OFT myOFT = OFT(contractAddress);
+    // function setReceiveConfig(address contractAddress, uint32 remoteEid, address receiveLibraryAddress, UlnConfig calldata ulnConfig) external {
+    //     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    //     OFT myOFT = OFT(contractAddress);
 
-        ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(address(myOFT.endpoint()));
+    //     ILayerZeroEndpointV2 endpoint = ILayerZeroEndpointV2(address(myOFT.endpoint()));
 
-        SetConfigParam[] memory setConfigParams = new SetConfigParam[](1);
-        setConfigParams[0] = SetConfigParam({
-            eid: remoteEid,
-            configType: RECEIVE_CONFIG_TYPE,
-            config: abi.encode(ulnConfig)
-        });
+    //     SetConfigParam[] memory setConfigParams = new SetConfigParam[](1);
+    //     setConfigParams[0] = SetConfigParam({
+    //         eid: remoteEid,
+    //         configType: RECEIVE_CONFIG_TYPE,
+    //         config: abi.encode(ulnConfig)
+    //     });
 
-        vm.startBroadcast(deployerPrivateKey);
+    //     vm.startBroadcast(deployerPrivateKey);
 
-        endpoint.setConfig(address(myOFT), receiveLibraryAddress, setConfigParams);
+    //     endpoint.setConfig(address(myOFT), receiveLibraryAddress, setConfigParams);
 
-        vm.stopBroadcast();
-    }
+    //     vm.stopBroadcast();
+    // }
     
 }
