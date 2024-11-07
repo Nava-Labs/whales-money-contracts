22e65a6be0de7d3d4f8db9d6835a30a211a36f1d
d1ceeth <erwinphanglius10@gmail.com>
2024-11-07 13:42:14 +0700

    fix: update mock staking contracts and scripts

--- script/MockStaking.s.sol
+++ script/MockStaking.s.sol
@@ -2,21 +2,41 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import {MockStaking2} from "../src/mock/Staking2.sol";
+import {MockStakingDirect,MockStakingWithRouter,MockStakingHop1,MockStakingHop2,MockStakingHop3} from "../src/mock/MockStaking.sol";
 
 contract DeployMockStaking is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        MockStaking2 _mock = new MockStaking2(
-            0x42fa6d207347a6c8472a5904E86310Bca48c85ac,
-            0xD76e2A1c4a1EB7328c742479F7D92847C493c986
+        address usdb = 0xe42c7D8eD302e12BD5E36c34bfc34188Abc9670f;
+
+        MockStakingDirect _mock1 = new MockStakingDirect(
+            usdb
         );
 
-        console.log(
-            "MockStaking2 contract deployed with address: ",
-            address(_mock)
+        address[] memory target = new address[](3);
+        target[0] = 0xD76e2A1c4a1EB7328c742479F7D92847C493c986;
+        target[1] = 0x7f1c3121E8578E406a5c245308AA1a141230435F;
+        target[2] = 0x000000000000000000000000000000000000dEaD;
+
+        MockStakingWithRouter _mock0 = new MockStakingWithRouter(
+            usdb,
+            target
+        );
+
+        MockStakingHop3 _hop3 = new MockStakingHop3(
+            usdb
+        );
+
+        MockStakingHop2 _hop2 = new MockStakingHop2(
+            usdb,
+            address(_hop3)
+        );
+
+        MockStakingHop1 _hop1 = new MockStakingHop1(
+            usdb,
+            address(_hop2)
         );
 
         vm.stopBroadcast();
---
+++ src/Mock/MockStaking.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MockStakingDirect { IERC20 public token;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function stake(uint256 amount) external {
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(address _to, uint256 _amount) external {
+        IERC20(token).transfer(_to, _amount);
+    }
+}
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IUsdb {
+    function deposit(address receiver, uint256 _amount) external;
+}
+
+contract MockStakingWithRouter {
+    IERC20 public usdb;
+
+    address[] public target;
+
+    constructor(address _usdb, address[] memory _target) {
+        usdb = IERC20(_usdb);
+        target = _target;
+    }
+
+    function routeUSDBtoTarget(uint256 amount) external {
+        IERC20(usdb).transferFrom(msg.sender, address(this), amount);
+
+        for(uint i; i < target.length; i++) {
+            IERC20(usdb).transfer(target[i], amount / target.length);
+        }
+    }
+
+    function withdraw(address _to, uint256 _amount) external {
+        IERC20(usdb).transfer(_to, _amount);
+    }
+}
+
+interface IHopper {
+    function hop(uint256 amount) external;
+}
+
+contract MockStakingHop1 {
+    IERC20 public usdb;
+
+    address public hop2;
+
+    constructor(address _usdb, address _hop2) {
+        usdb = IERC20(_usdb);
+        hop2 = _hop2;
+
+        IERC20(usdb).approve(hop2, type(uint256).max);        
+    }
+
+    function routeUSDBtoHop2(uint256 amount) external {
+        IERC20(usdb).transferFrom(msg.sender, address(this), amount);
+        IHopper(hop2).hop(amount);
+    }
+}
+
+contract MockStakingHop2 is IHopper {
+    IERC20 public usdb;
+
+    address public hop3;
+
+    constructor(address _usdb, address _hop3) {
+        usdb = IERC20(_usdb);
+        hop3 = _hop3;
+
+        IERC20(usdb).approve(hop3, type(uint256).max);        
+    }
+
+    function hop(uint256 amount) external {
+        IERC20(usdb).transferFrom(msg.sender, address(this), amount);
+        IHopper(hop3).hop(amount);
+    }
+}
+
+contract MockStakingHop3 { 
+    IERC20 public usdb;
+
+    constructor(address _usdb) {
+        usdb = IERC20(_usdb);
+    }
+
+
+    function hop(uint256 amount) external {
+        IERC20(usdb).transferFrom(msg.sender, address(this), amount);
+    }
+}
